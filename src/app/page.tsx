26ae959e6842a826b0e53f097a1a9acec0b75f4b
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2024-06-10 21:10:15 +0700

    2.Code layout Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-	return (
-		<main>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-			debitis doloribus necessitatibus cum est officia non dolore porro nostrum,
-			facilis reiciendis rem sit, temporibus quos excepturi quas eveniet
-			quisquam. Minus.
-		</main>
-	);
+	return <div>home</div>;
 }
---
+++ src/components/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as IconPlay } from './icon-play';
+export { default as IconExplore } from './icon-explore';
