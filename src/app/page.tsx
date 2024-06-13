6560e63d08ff8fdefe98399f2fc717b164b8b252
Nass <anhnamnguyuen0203@gmail.com>
2024-06-14 06:13:38 +0700

    4.Kết nối Database trong NextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
+import { connectToDatabase } from '@/lib/mongoose';
+
+export default async function Home() {
+	const connect = connectToDatabase();
 	return <div>Home Page</div>;
 }
